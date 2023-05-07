71d5b020d7e6fee414fec6294ea2c91ba083b0ed
Uwe Loock <uwe.loock@stud.thga.de>
2023-05-07 10:29:00 +0200

    pinke Katze zugefügt

--- ansi_color.c
+++ ansi_color.c
@@ -12,4 +12,19 @@ int main(){
 
 	printf( "\n \x1b[31;1m %s \x1b[0m\n",ascii_art );
 
+{
+  // ANSI-Escape-Codes verwenden, um die Ausgabe farbig zu gestalten
+  
+	printf("\033[2J"); // Bildschirm mit ANSI-Escape-Codes leeren
+	printf("\033[1;35m"); // Pink färben
+	
+      	printf("     /\\_/\\   \n");
+  	printf("    ( o.o )  \n");
+  	printf("     > ^ <   \n");
+  	printf("\033[0m"); // Farbe zurücksetzen
+
+  return 0;
+}
+
+
 }	
